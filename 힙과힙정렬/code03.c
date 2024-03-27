3c30eb6214b4196fea656c9e30f31c75b8fcf561
이하연 <papepopepe@naver.com>
2024-03-27 10:47:03 +0900

    Create code03.c

---
+++ 힙과힙정렬/code03.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+void rBuildHeap(int H[], int i, int n);
+void downHeap(int H[], int i, int n);
+void printHeap(int H[], int n);
+
+int main()
+{
+	int n = 0, *H = NULL;
+
+	scanf("%d", &n);
+	H = (int *)malloc(sizeof(int)*(n+1));
+	H[0] = 0;
+
+	for (int i = 1; i <= n; i++) scanf(" %d", &H[i]);
+	rBuildHeap(H, 1, n);
+	printHeap(H, n);
+
+	free(H);
+	return 0;
+}
+
+void downHeap(int H[], int i, int n)
+{
+	int left = 2 * i, right = 2 * i + 1;
+	int temp = 0;
+
+	if (left > n) return;
+
+	int small = left;
+
+	if (right <= n) {
+		if (H[right] > H[small]) small = right;
+	}
+	
+	if (H[i] <= H[small]) {
+		temp = H[small];
+		H[small] = H[i];
+		H[i] = temp;
+	}
+
+	downHeap(H, small, n);
+}
+
+void rBuildHeap(int H[], int i, int n) {
+	if (i > n) return;
+
+	rBuildHeap(H, 2 * i, n);
+	rBuildHeap(H, 2 * i + 1, n);
+	downHeap(H, i, n);
+	return;
+}
+
+void printHeap(int H[], int n)
+{
+	for (int i = 1; i <= n; i++) printf(" %d", H[i]);
+	printf("\n");
+}
